03c915f52288c910a311eefbcdf932b64136db54
moosejaw <joshdemir@gmail.com>
2020-10-29 18:02:16 +0000

    Enumerate different algorithm types

--- src/com/moosejaw/pathfinder/alg/config/Algorithm.java
+++ src/com/moosejaw/pathfinder/alg/config/Algorithm.java
@@ -1,2 +1,7 @@
-package com.moosejaw.pathfinder.alg.config;public enum Algorithm {
+package com.moosejaw.pathfinder.alg.config;
+
+// TODO: Fill this out and attach to a combobox in the main toolbar
+
+public enum Algorithm {
+    DJIKSTRA;
 }
